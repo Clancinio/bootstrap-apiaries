1a8ab4a201d05a2672816fd3a7d4b89f8151091f
Dean <dean.clancy91@gmail.com>
2019-10-18 15:34:30 -0700

    Added responsive navigation

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function openSideMenu() {
+    document.getElementById("side-menu").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "50px";
+
+}
+
+function closeSideMenu() {
+    document.getElementById("side-menu").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+
+}
